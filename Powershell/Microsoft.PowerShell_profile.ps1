337631e4112863ac0480ba5115198b39a824a4cf
Arne Halvorsen <arne.halvorsen@gmail.com>
2024-07-07 00:06:07 +0200

    renamed file loc-fun.ps1 to loc-cli.ps1 since main feature is to implement a cli

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -53,7 +53,7 @@ function help {
     Write-Host "    fs-help:                        Show file system functions"
     Write-Host "    ut-help:                        Show utility functions"
     Write-Host "    ji-help:                        Show Jira functions"
-    Write-Host "    loc-help:                       Show location management and navigation commands help"
+    Write-Host "    loc-help:                       Show location management and navigation commands help, the loc cli"
     Write-Host
 }
 
