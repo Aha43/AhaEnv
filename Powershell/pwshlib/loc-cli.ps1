110551a1b5e5b0bd2052df239c39cc1b5bdfba33
Arne Halvorsen <arne.halvorsen@gmail.com>
2024-07-09 19:27:01 +0200

    Moved functions that writes help to internal section

--- Powershell/pwshlib/loc-cli.ps1
+++ Powershell/pwshlib/loc-cli.ps1
@@ -112,6 +112,123 @@ function Get-Timestamp {
     return (Get-Date).ToString("yyyyMMddHHmmss")
 }
 
+function Get-LocAddHelp {
+    Write-Host
+    Write-Host "Usage: loc add <name> <description>" -ForegroundColor Green
+    Write-Host "Add the current working directory as a location with the given name and description" -ForegroundColor Green
+    Write-Host
+}
+
+function Get-LocNoteHelp {
+    Write-Host
+    Write-Host "Usage: loc note <name> <note>" -ForegroundColor Green
+    Write-Host "Add a note to the location with the given name" -ForegroundColor Green
+    Write-Host
+}
+
+function Get-LocNotesHelp {
+    Write-Host
+    Write-Host "Usage: loc notes <name>" -ForegroundColor Green
+    Write-Host "Show notes for the location with the given name" -ForegroundColor Green
+    Write-Host
+}
+
+function Get-LocUpdateHelp {
+    Write-Host
+    Write-Host "Usage: loc update <name>" -ForegroundColor Green
+    Write-Host "Update the path of a location with the given name to the current working directory" -ForegroundColor Green
+    Write-Host
+}
+
+function Get-LocRenameHelp {
+    Write-Host
+    Write-Host "Usage: loc rename <name> <new-name>" -ForegroundColor Green
+    Write-Host "Rename a location with the given name to the new name" -ForegroundColor Green
+    Write-Host
+}
+
+function Get-LocEditHelp {
+    Write-Host
+    Write-Host "Usage: loc edit <name> <description>" -ForegroundColor Green
+    Write-Host "Edit the description of a location with the given name" -ForegroundColor Green
+    Write-Host
+}
+
+function Get-LocListHelp {
+    Write-Host
+    Write-Host "Usage: loc list" -ForegroundColor Green
+    Write-Host "List all locations" -ForegroundColor Green
+    Write-Host "You can also use 'loc ls' or 'loc l' to list all locations" -ForegroundColor Green
+    Write-Host
+}
+
+function Get-LocRemoveHelp {
+    Write-Host
+    Write-Host "Usage: loc remove <name>" -ForegroundColor Green
+    Write-Host "Remove a location with the given name" -ForegroundColor Green
+    Write-Host
+}
+
+function Get-LocRemoveThisHelp {
+    Write-Host
+    Write-Host "Usage: loc remove-this" -ForegroundColor Green
+    Write-Host "Remove the location you are currently at (do not worry the physical directory not deleted)" -ForegroundColor Green
+    Write-Host
+}
+
+function Get-LocRepairHelp {
+    Write-Host
+    Write-Host "Usage: loc repair" -ForegroundColor Green
+    Write-Host "Remove locations that do not physically exist" -ForegroundColor Green
+    Write-Host
+}
+
+function Get-LocGotoHelp {
+    Write-Host
+    Write-Host "Usage: loc goto <name | pos>" -ForegroundColor Green
+    Write-Host "Go to the location with the given name (or position in location list)" -ForegroundColor Green
+    Write-Host "You can also use 'loc go <name | pos>' to go to a location" -ForegroundColor Green
+    Write-Host "Finally, you can use 'loc <name | pos>' to go to a location" -ForegroundColor Green
+    Write-Host
+}
+
+function Get-LocWhereHelp {
+    Write-Host
+    Write-Host "Usage: loc where" -ForegroundColor Green
+    Write-Host "Show the location you are currently at" -ForegroundColor Green
+    Write-Host
+}
+
+function Get-LocCliActions {
+    $commands = @(
+        "add",
+        "note",
+        "notes",
+        "update",
+        "rename",
+        "edit",
+        "list",
+        "remove",
+        "remove-this",
+        "repair",
+        "goto",
+        "where"
+    )
+    return $commands
+}
+
+function Get-LocCliHelp {
+    $actions = (Get-LocCliActions) -join ", "
+    Write-Host
+    Write-Host "loc - A location management and navigation command line interface" -ForegroundColor Green
+    Write-Host 
+    Write-Host "Usage: loc <action> ..." -ForegroundColor Green
+    Write-Host "Actions: $actions" -ForegroundColor Green
+    Write-Host
+    Write-Host "Use 'loc help <action>' for more information on a specific action" -ForegroundColor Green
+    Write-Host
+}
+
 # Exported functions
 
 function Add-Location {
@@ -394,122 +511,7 @@ function Get-LocationWhereIAm {
     }
 }
 
-function Get-LocAddHelp {
-    Write-Host
-    Write-Host "Usage: loc add <name> <description>" -ForegroundColor Green
-    Write-Host "Add the current working directory as a location with the given name and description" -ForegroundColor Green
-    Write-Host
-}
 
-function Get-LocNoteHelp {
-    Write-Host
-    Write-Host "Usage: loc note <name> <note>" -ForegroundColor Green
-    Write-Host "Add a note to the location with the given name" -ForegroundColor Green
-    Write-Host
-}
-
-function Get-LocNotesHelp {
-    Write-Host
-    Write-Host "Usage: loc notes <name>" -ForegroundColor Green
-    Write-Host "Show notes for the location with the given name" -ForegroundColor Green
-    Write-Host
-}
-
-function Get-LocUpdateHelp {
-    Write-Host
-    Write-Host "Usage: loc update <name>" -ForegroundColor Green
-    Write-Host "Update the path of a location with the given name to the current working directory" -ForegroundColor Green
-    Write-Host
-}
-
-function Get-LocRenameHelp {
-    Write-Host
-    Write-Host "Usage: loc rename <name> <new-name>" -ForegroundColor Green
-    Write-Host "Rename a location with the given name to the new name" -ForegroundColor Green
-    Write-Host
-}
-
-function Get-LocEditHelp {
-    Write-Host
-    Write-Host "Usage: loc edit <name> <description>" -ForegroundColor Green
-    Write-Host "Edit the description of a location with the given name" -ForegroundColor Green
-    Write-Host
-}
-
-function Get-LocListHelp {
-    Write-Host
-    Write-Host "Usage: loc list" -ForegroundColor Green
-    Write-Host "List all locations" -ForegroundColor Green
-    Write-Host "You can also use 'loc ls' or 'loc l' to list all locations" -ForegroundColor Green
-    Write-Host
-}
-
-function Get-LocRemoveHelp {
-    Write-Host
-    Write-Host "Usage: loc remove <name>" -ForegroundColor Green
-    Write-Host "Remove a location with the given name" -ForegroundColor Green
-    Write-Host
-}
-
-function Get-LocRemoveThisHelp {
-    Write-Host
-    Write-Host "Usage: loc remove-this" -ForegroundColor Green
-    Write-Host "Remove the location you are currently at (do not worry the physical directory not deleted)" -ForegroundColor Green
-    Write-Host
-}
-
-function Get-LocRepairHelp {
-    Write-Host
-    Write-Host "Usage: loc repair" -ForegroundColor Green
-    Write-Host "Remove locations that do not physically exist" -ForegroundColor Green
-    Write-Host
-}
-
-function Get-LocGotoHelp {
-    Write-Host
-    Write-Host "Usage: loc goto <name | pos>" -ForegroundColor Green
-    Write-Host "Go to the location with the given name (or position in location list)" -ForegroundColor Green
-    Write-Host "You can also use 'loc go <name | pos>' to go to a location" -ForegroundColor Green
-    Write-Host "Finally, you can use 'loc <name | pos>' to go to a location" -ForegroundColor Green
-    Write-Host
-}
-
-function Get-LocWhereHelp {
-    Write-Host
-    Write-Host "Usage: loc where" -ForegroundColor Green
-    Write-Host "Show the location you are currently at" -ForegroundColor Green
-    Write-Host
-}
-
-function Get-LocCliActions {
-    $commands = @(
-        "add",
-        "note",
-        "notes",
-        "update",
-        "rename",
-        "edit",
-        "list",
-        "remove",
-        "remove-this",
-        "repair",
-        "goto",
-        "where"
-    )
-    return $commands
-}
-
-function Get-LocCliHelp {
-    $actions = (Get-LocCliActions) -join ", "
-    Write-Host
-    Write-Host "loc - A location management and navigation command line interface" -ForegroundColor Green
-    Write-Host 
-    Write-Host "Usage: loc <action> ..." -ForegroundColor Green
-    Write-Host "Actions: $actions" -ForegroundColor Green
-    Write-Host
-    Write-Host "Use 'loc help <action>' for more information on a specific action" -ForegroundColor Green
-    Write-Host
-}
 
 # cli
 function Loc {
