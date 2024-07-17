8ba9bde84365262af88188f66a9f5b1482c3c49d
Arne Halvorsen <arne.halvorsen@gmail.com>
2024-07-17 02:25:47 +0200

    main catch up

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -16,6 +16,16 @@ if (Test-Path $LibDir) {
     Write-Host "Directory not found: $LibDir" -ForegroundColor Red
 }
 
+# Loading user specified stuff
+function Run-UserPsFile {
+    $UserPsFile = Join-Path -Path $HOME -ChildPath ".myps.ps1"
+    Write-Host "UserPsFile: $UserPsFile"
+    if (Test-Path $UserPsFile) {
+        Write-Host "Running user specified file: $UserPsFile"
+        . $UserPsFile
+    }
+}
+
 function _promptheader {
     $Date = Get-Date -Format "dd.MM.yy"
     $Week = week
@@ -344,4 +354,8 @@ function prompt {
     return " "
 }
 
+<<<<<<< HEAD
 Set-PSReadLineOption -Colors @{ Command = 'Green' }
+=======
+Run-UserPsFile
+>>>>>>> 8f1e451 (now run a file .myps.ps1 if exist in users home dir)
