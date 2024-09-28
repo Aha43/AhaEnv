fb274d063d1fee5ca4f37d6bf30b62d45cba62e5
Arne Halvorsen <arne.halvorsen@scaleaq.com>
2024-09-28 13:40:28 +0200

    added function to create self signed certificate

--- Powershell/pwshlib/dev-fun.ps1
+++ Powershell/pwshlib/dev-fun.ps1
@@ -6,6 +6,7 @@ function dev-help {
     Write-Host "Development functions:"
     Write-Host "    dev-help:                       Show this help"
     Write-Host "    csln:                           Clear a solution of all build artifacts"
+    Write-Host "    create-self-signed-cert:        Create a self-signed certificate for localhost"
     Write-Host
 }
 
@@ -18,4 +19,22 @@ function csln {
 
     Get-ChildItem -Recurse -Filter "obj" | Remove-Item -Recurse -Force
     Get-ChildItem -Recurse -Filter "bin" | Remove-Item -Recurse -Force
+}
+
+function create-self-signed-cert {
+    $certName = "localhost"
+    $certPassword = "at_starbucks-basar"
+    $certPath = "~/certs/$certName.pfx"
+
+    if (-not (Test-Path "certs")) {
+        New-Item -ItemType Directory -Path "certs"
+    }
+
+    if (Test-Path $certPath) {
+        Write-Host "Certificate already exists at $certPath"
+        return
+    }
+
+    $cert = New-SelfSignedCertificate -DnsName $certName -CertStoreLocation "cert:\LocalMachine\My" -KeyExportPolicy Exportable -KeySpec Signature
+    $cert | Export-PfxCertificate -FilePath $certPath -Password (ConvertTo-SecureString -String $certPassword -Force -AsPlainText)
 }
